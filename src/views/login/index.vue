6b6613771209a4b65d8a765472d698841f66914f
贾旭光 <2860905270@qq.com>
2020-02-10 21:00:27 +0800

    登录模块，数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,23 +6,25 @@
               <img src="../../assets/logo_index.png" alt="">
           </div>
           <!-- 表单 -->
-          <el-form style="margin-top:30px">
+          <!-- model属性要绑定表单数据对象 -->
+          <!-- rules校验规则对象 -->
+          <el-form style="margin-top:30px" :model="loginForm" :rules='loginRules' ref="formObj">
               <!-- 手机号 -->
-              <el-form-item>
-                  <el-input placeholder="请输入手机号"></el-input>
+              <el-form-item prop="mobile">
+                  <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
               </el-form-item>
               <!-- 验证码及其按钮 -->
-              <el-form-item>
-                  <el-input placeholder="请输入验证码" style="width:240px"></el-input>
+              <el-form-item prop="code">
+                  <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
                   <el-button plain style="float:right">发送验证码</el-button>
               </el-form-item>
               <!-- 用户须知同意框 -->
-              <el-form-item>
-                   <el-checkbox> 我已阅读并同意用户协议和隐私条款 </el-checkbox>
+              <el-form-item prop="checked">
+                   <el-checkbox v-model="loginForm.checked"> 我已阅读并同意用户协议和隐私条款 </el-checkbox>
               </el-form-item>
               <!-- 登录按钮 -->
               <el-form-item>
-                  <el-button type="primary" style="width:100%">登录</el-button>
+                  <el-button @click="login" type="primary" style="width:100%">登录</el-button>
               </el-form-item>
           </el-form>
       </el-card>
@@ -31,7 +33,50 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 要校验的整个表单数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否勾选
+      },
+      //    校验规则对象
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号' }
+        ],
+        code: [
+          { required: true, message: '请输入您的验证码' },
+          { pattern: /^\d{6}$/, message: '验证码错误' }
+        ],
+        // rule代表当前规则，没什么用/value代表当前的值=>checked的值/callback回调函数
+        checked: [{
+          validator: function (rules, value, callback) {
+            if (value) {
+            // 如果是true，表示选中直接执行callback
+              callback()
+            } else {
+            //   不通过也直接执行callback
+              callback(new Error('您必须同意此条款'))
+            }
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // this.$refs.formObj获取el-form的对象实例
+      this.$refs.formObj.validate(function (isOk) {
+        if (isOk) {
+          // 如果为true弹出登录成功
+          alert('登录成功')
+        }
+      })
+    }
+  }
 }
 </script>
 
