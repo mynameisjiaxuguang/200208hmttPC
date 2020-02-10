bc38b81a5becc258f0e34b0b29c183753a2fd37e
贾旭光 <2860905270@qq.com>
2020-02-10 16:41:03 +0800

    登录模块，页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>登录</div>
+  <div class="login">
+      <el-card class="login-card">
+          <!-- 黑马头条图片 -->
+          <div class="title">
+              <img src="../../assets/logo_index.png" alt="">
+          </div>
+          <!-- 表单 -->
+          <el-form style="margin-top:30px">
+              <!-- 手机号 -->
+              <el-form-item>
+                  <el-input placeholder="请输入手机号"></el-input>
+              </el-form-item>
+              <!-- 验证码及其按钮 -->
+              <el-form-item>
+                  <el-input placeholder="请输入验证码" style="width:240px"></el-input>
+                  <el-button plain style="float:right">发送验证码</el-button>
+              </el-form-item>
+              <!-- 用户须知同意框 -->
+              <el-form-item>
+                   <el-checkbox> 我已阅读并同意用户协议和隐私条款 </el-checkbox>
+              </el-form-item>
+              <!-- 登录按钮 -->
+              <el-form-item>
+                  <el-button type="primary" style="width:100%">登录</el-button>
+              </el-form-item>
+          </el-form>
+      </el-card>
+    </div>
 </template>
 
 <script>
@@ -8,6 +35,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+    .login {
+        height: 100vh;
+        background: url('../../assets/login_bg.jpg');
+        background-size: cover;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .login-card {
+            width: 400px;
+            height: 360px;
+        }
+        .title {
+            text-align: center;
+            img {
+                height: 45px;
+            }
+        }
+    }
 </style>
