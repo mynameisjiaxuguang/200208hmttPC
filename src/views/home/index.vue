cf5982ee909233dc1e5fc43662ac42c17ef7150f
贾旭光 <2860905270@qq.com>
2020-02-11 12:16:09 +0800

    主页模块页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    主页
+    <!-- 布局 -->
+    <el-container>
+      <!-- 左右布局 -->
+  <el-aside width="200px">
+    <div>
+      <img src="../../assets/logo_admin.png" alt="">
+    </div>
+  </el-aside>
+  <!-- 上下布局 -->
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>
+      <!-- 二级路由容器 -->
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</el-container>
   </div>
 </template>
 
